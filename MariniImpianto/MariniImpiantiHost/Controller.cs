212063db6e9316faf8cf2760fe549bd69a69312a
rmantovani68 <rmantovani68@gmail.com>
2017-05-23 10:16:51 +0200

    Nuova versione xsd e xml

--- MariniImpianto/MariniImpiantiHost/Controller.cs
+++ MariniImpianto/MariniImpiantiHost/Controller.cs
@@ -327,18 +327,6 @@ namespace MariniImpiantiHost
             return RetVal;
         }
 
-        // reperisce il default ip address dai settings
-        public string GetDefaultIpAddress()
-        {
-            return Settings.Default.IpAddress;
-        }
-        
-        // reperisce il default plc name dai settings
-        public string GetDefaultPLCName()
-        {
-            return Settings.Default.PLCName;
-        }
-
         #endregion Public Methods
 
         #region Private Methods
